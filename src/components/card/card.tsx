3aaf45317c721783853fe20f0c385febe52fe565
Gilad T <2517870+giladt@users.noreply.github.com>
2023-06-20 23:53:04 +0200

    chore: removed apartment info from cards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,19 +1,15 @@
 "use client";
-import { useRef, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import { v4 as uuidv4 } from "uuid";
-import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { Dancing_Script } from "next/font/google";
-import * as ReactMdIcon from "react-icons/md";
+import Image from "next/image";
+import Link from "next/link";
 
-import { Dialog } from "@/components/dialog/dialog";
-import { Calendar } from "@/components/calendar/calendar";
 import { wfImageUrl } from "@/utils/images";
-import { TBookings, TMdIcons } from "@/utils/types.d";
 
 import styles from "./card.module.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { TbCalendar } from "react-icons/tb";
 
 const dancing = Dancing_Script({
   subsets: ["latin"],
@@ -30,96 +26,50 @@ type TCardProps = {
     description?: string;
     priority?: boolean;
   }[];
-  tags?: { icon: TMdIcons; text: string }[];
-  bookings?: TBookings;
 };
 
 export default function Card({ url, name, info, images }: TCardProps) {
+  const router = useRouter();
 
   return (
-    <>
-      <section className={`${styles.card} bg-slate-100 dark:bg-slate-900`}>
-        <Carousel
-          className={styles.card__gallery}
-          ariaLabel={`Apartment ${name} photos.`}
-          showThumbs={false}
-          showIndicators={false}
-          showStatus={false}
-          showArrows={true}
-          onClickItem={(index: number) => {
-            setCurrentImage(index);
-            refDialogGallery.current?.showModal();
-          }}
-          emulateTouch={true}
-          useKeyboardArrows
+    <section className={styles.card}>
+      <Carousel
+        className={styles.card__gallery}
+        ariaLabel={`Apartment ${name} photos.`}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        showArrows={true}
+        onClickItem={(index: number) => {
+          router.push(`/${url}?img=${index}`);
+        }}
+        emulateTouch={true}
+        useKeyboardArrows
+      >
+        {images.map((image) => (
+          <Image
+            key={uuidv4()}
+            src={wfImageUrl(image.id, "th")}
+            alt={image.description || ""}
+            width={320}
+            height={320}
+            priority={image.priority || false}
+          />
+        ))}
+      </Carousel>
+      <div className={styles.card__info}>
+        <h3 className={dancing.className}>{name}</h3>
+        {info && <p>{info}</p>}
+        <Link
+          role="button"
+          type="button"
+          className="mt-5 btn-root outline outline-teal-300 hover:bg-teal-300 focus:bg-teal-400
+          dark:outline-teal-700 dark:hover:bg-teal-700 dark:focus:bg-teal-600"
+          href={url}
         >
-          {images.map((image) => (
-            <Image
-              key={uuidv4()}
-              src={wfImageUrl(image.id, "th")}
-              alt={image.description || ""}
-              width={320}
-              height={320}
-              priority={image.priority || false}
-            />
-          ))}
-        </Carousel>
-        <div className={styles.card__info}>
-          <h3 className={dancing.className}>{name}</h3>
-          {info && <p>{info}</p>}
-          {tags && (
-            <div className={styles.tags}>
-              {tags.map(
-                ({ icon, text }: { icon: TMdIcons; text: string }, index) => {
-                  const MdIcon = ReactMdIcon[icon];
-                  return (
-                    <span key={`tag-${index}`} className={styles.tag}>
-                      <MdIcon className={styles.tag__icon} />
-                      <div className={styles.tag__text}>{text}</div>
-                    </span>
-                  );
-                }
-              )}
-            </div>
-          )}
-          <button
-            className="btn-root bg-teal-800 text-teal-50 hover:bg-teal-700 focus:bg-teal-600
-            dark:bg-teal-200 dark:text-teal-950 dark:hover:bg-teal-300 dark:focus:bg-teal-400"
-            onClick={() => refDialogCalendar.current?.showModal()}
-          >
-            <TbCalendar /> Check Availability
-          </button>
-        </div>
-      </section>
-      <Dialog ref={refDialogCalendar} type="calendar">
-        <h3>{name} Availability</h3>
-        <Calendar bookings={bookings} />
-      </Dialog>
-      <Dialog ref={refDialogGallery} type="gallery">
-        <Carousel
-          className="max-[80%]"
-          ariaLabel={`Apartment ${name} photos.`}
-          showThumbs={false}
-          showIndicators={true}
-          showArrows={true}
-          centerMode={false}
-          showStatus={false}
-          emulateTouch={true}
-          selectedItem={currentImage || 0}
-          useKeyboardArrows
-        >
-          {images.map((image) => (
-            <div
-              key={uuidv4()}
-              className={styles.card__fs_gallery_image}
-              style={{
-                backgroundImage: `url(${wfImageUrl(image.id, "lg")})`,
-              }}
-              aria-label={image.description || ""}
-            />
-          ))}
-        </Carousel>
-      </Dialog>
-    </>
+          More details
+        </Link>
+      </div>
+    </section>
   );
 }
--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,23 +1,9 @@
 import Card from "@/components/card/card";
 import styles from "./cards.module.scss";
-import apartmentsData from "@/assets/apartments.json";
-import { getData } from "@/utils/data-handlers";
-import { IApartmentData } from "@/utils/types.d";
+import apartments from "@/assets/apartments.json";
 
 type TCardsProps = {};
 export default async function Cards({}: TCardsProps) {
-  const apartments: IApartmentData[] = [];
-
-  for (let apartment of apartmentsData as IApartmentData[]) {
-    const url = `https://www.airbnb.com/calendar/ical/${
-      process.env["ICAL_ID_APT_" + apartment.id]
-    }.ics?s=${process.env["ICAL_SECRET_APT_" + apartment.id]}`;
-    const data = await getData(url);
-    const bookings = await JSON.parse(JSON.stringify(data));
-
-    apartments.push({ ...apartment, bookings });
-  }
-
   return (
     <section
       id="cards"
