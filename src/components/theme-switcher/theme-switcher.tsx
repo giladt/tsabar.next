f798b0a96a761ab55290e26cac253a57201f479e
Gilad T <2517870+giladt@users.noreply.github.com>
2023-06-20 23:54:32 +0200

    chore: few style tweaks

--- src/components/theme-switcher/theme-switcher.tsx
+++ src/components/theme-switcher/theme-switcher.tsx
@@ -16,16 +16,23 @@ export const ThemeSwitcher = () => {
   if (!mounted) return null;
 
   return (
-    <span className="fixed top-2 right-2" aria-label="Switch color theme">
+    <span
+      className="fixed top-2 right-2 p-1
+      rounded-full z-10
+      bg-white/50 dark:bg-black/50
+      hover:bg-white/75 dark:hover:bg-black/75
+      "
+      aria-label="Switch color theme"
+    >
       {(currentTheme === "dark" && (
         <TbBulb
-          className="w-7 h-7 text-gray-100"
+          className="w-7 h-7 text-white"
           role="button"
           onClick={() => setTheme("light")}
         />
       )) || (
         <TbBulbOff
-          className="w-7 h-7 text-gray-900 "
+          className="w-7 h-7 text-black"
           role="button"
           onClick={() => setTheme("dark")}
         />
