9433a0dd907a0b8d80347f30cd0bf01b8ceb5e5e
Gilad T <2517870+giladt@users.noreply.github.com>
2023-06-25 23:22:40 +0200

    fix: extend tw colors correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,17 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
   ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-light": "var(--primary-light)",
+        "secondary-light": "var(--secondary-light)",
+        "tertiary-light": "var(--tertiary-light)",
+        "primary-dark": "var(--primary-dark)",
+        "secondary-dark": "var(--secondary-dark)",
+        "tertiary-dark": "var(--tertiary-dark)",
+      },
+    },
+  },
   darkMode: "class",
 }
