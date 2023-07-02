298897a87a19a8bbef917be04cfe319ff777450d
Gilad T <2517870+giladt@users.noreply.github.com>
2023-07-02 17:27:09 +0200

    chore: added 'about' page. CMS from notion

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,29 @@
-function About() {
-  return <div>About</div>;
+import Link from "next/link";
+import { MdArrowBack } from "react-icons/md";
+import styles from "./about.module.scss";
+
+const fetchFromNotion = async () => {
+  const res = await fetch("http://localhost:3000/api/notion", {
+    next: { revalidate: 60 },
+  });
+  const data = await res.json();
+  return data;
+};
+
+async function About() {
+  const data = await fetchFromNotion();
+  // const pageData = await getPageData(data, "about");
+  console.log({ data });
+
+  return (
+    <div className={styles.about}>
+      <Link href="/">
+        <MdArrowBack />
+        Back home
+      </Link>
+      <br />
+      <main dangerouslySetInnerHTML={{ __html: data.data }} />
+    </div>
+  );
 }
 export default About;
