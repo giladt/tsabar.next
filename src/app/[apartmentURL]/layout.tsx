b2506c9a0b3b5c097753f722616ded14cfe738f2
Gilad T <2517870+giladt@users.noreply.github.com>
2023-07-04 23:50:03 +0200

    fix: re-added missing back button

--- src/app/[apartmentURL]/layout.tsx
+++ src/app/[apartmentURL]/layout.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/button";
 import { type ReactNode } from "react";
+import { MdHouse } from "react-icons/md";
 
 export default function ApartmentLayout({ children }: { children: ReactNode }) {
-  return <div className="m-0">{children}</div>;
+  return (
+    <div className="m-0">
+      <Button.Back href="/">
+        <MdHouse />
+      </Button.Back>
+      {children}
+    </div>
+  );
 }
