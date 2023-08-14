5317f4af6af672a74a0626ab447bfc54d28bdf42
Gilad T <2517870+giladt@users.noreply.github.com>
2023-08-14 23:41:33 +0200

    chore: anonymized GA ip

--- src/components/google/analytics/index.tsx
+++ src/components/google/analytics/index.tsx
@@ -42,7 +42,8 @@ export default function GoogleAnalytics({
             });
 
             gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
+              'page_path': window.location.pathname,
+              'anonymize_ip': true,
             })
           `,
         }}
